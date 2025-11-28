89d7f55a9ffff9de2e1c43154604cc6cda0f1629
kirill <kirillkhromov@mail.ru>
2025-11-29 00:55:17 +0300

    annotation @SuppressWarnings @Deprecated

--- src/main/java/org/example/Anotation/An.java
+++ src/main/java/org/example/Anotation/An.java
@@ -1,4 +1,30 @@
 package org.example.Anotation;
 
-public class An {
+import java.util.ArrayList;
+import java.util.List;
+
+public class An  extends  A{
+    public static void main(String[] args) {
+        A a = new A();
+
+        a.addValues();
+        a.MyMethod();
+    }
+}
+
+class A{
+    public void tes(){
+        System.out.println("hello from B");
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void addValues() {
+        List list = new ArrayList(); // raw type
+        list.add("hello");
+        list.add(123);
+    }
+    @Deprecated
+    public void MyMethod(){
+        System.out.println("other method ");
+    }
 }

57dca60f83b208dcdd57684abea23c01d64f44f2
kirill <kirillkhromov@mail.ru>
2025-12-22 23:19:46 +0300

    null

--- src/main/java/org/example/forkjoin/forkjoin.java
+++ src/main/java/org/example/forkjoin/forkjoin.java
@@ -17,7 +17,7 @@ public class forkjoin {
 
         int result = fjp.invoke(task);  // для форк джоина дает задачу
         System.out.println(result); // выводим результат
-        System.out.println("");
+        
 
     }
 }
